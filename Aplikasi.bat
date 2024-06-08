1a5a04524ebd6a681dfc0a5fc2ca4c654e8bccff
Cahya Revanto <64825576+revan2159@users.noreply.github.com>
2024-06-08 08:10:16 +0700

    s

--- Aplikasi.bat
+++ Aplikasi.bat
@@ -1,1 +1,4 @@
+git stash
+git clean -df
+git pull origin main
 java -jar -Xss2m -Xms32m -Xmx1024m -XX:PermSize=32m -XX:MaxPermSize=1024m khanza.jar
---
+++ Update.bat
@@ -0,0 +1,4 @@
+git stash
+git clean -df
+git pull origin main
+
