33bd0a4c7091c85c69facb5642e1f31ca1bd8d5f
Cahya Revanto <64825576+revan2159@users.noreply.github.com>
2024-06-08 09:24:28 +0700

    h

--- Install.bat
+++
@@ -1,9 +0,0 @@
-setlocal EnableDelayedExpansion
-set "userFolder=C:\Users\%username%"
-
-if not exist "!userFolder!\Dokumen" (
-    md "!userFolder!\Tes"
-    echo Folder "Dokumen" dibuat untuk pengguna %username%
-)
-
-git clone https://github.com/revan2159/SIMRSKhanza.git SIMRSKhanza
--- installKhanza.cmd
+++ installKhanza.cmd
@@ -22,20 +22,11 @@ if exist D:\ (
   git clone https://github.com/revan2159/SIMRSKhanza.git D:\SIMRSKhanza
   if %errorlevel% == 0 (
     echo Repository cloned successfully to drive D:!
-    echo Creating desktop shortcut...
     echo %userFolder%\Desktop\Aplikasi.bat
-    echo "D:\SIMRSKhanza\Aplikasi.bat"
-    echo "Working Directory: D:\SIMRSKhanza"
     echo "Show in New Window"
     echo.
-    mklink /h D:\Desktop\Aplikasi.bat D:\SIMRSKhanza\Aplikasi.bat
-    if %errorlevel% == 0 (
-      echo Shortcut created successfully!
-      pause
-    ) else (
-      echo Shortcut creation failed! Error code: %errorlevel%
-      pause
-    )
+    pause
+    start explorer D:\SIMRSKhanza\
   ) else (
     echo Repository cloning failed! Error code: %errorlevel%
     pause
@@ -46,20 +37,11 @@ if exist D:\ (
   git clone https://github.com/revan2159/SIMRSKhanza.git E:\SIMRSKhanza
   if %errorlevel% == 0 (
     echo Repository cloned successfully to drive E:!
-    echo Creating desktop shortcut...
-    echo %userFolder%\Desktop\Aplikasi.bat
-    echo "E:\SIMRSKhanza\Aplikasi.bat"
     echo "Working Directory: E:\SIMRSKhanza"
     echo "Show in New Window"
     echo.
-    mklink /h "%userFolder%\Desktop\Aplikasi.bat" "E:\SIMRSKhanza\Aplikasi.bat"
-    if %errorlevel% == 0 (
-      echo Shortcut created successfully!
-      pause
-    ) else (
-      echo Shortcut creation failed! Error code: %errorlevel%
-      pause
-    )
+    pause
+    start explorer E:\SIMRSKhanza\
   ) else (
     echo Repository cloning failed! Error code: %errorlevel%
     pause
@@ -70,21 +52,12 @@ if exist D:\ (
   git clone https://github.com/revan2159/SIMRSKhanza.git "!userFolder!\SIMRSKhanza"
   if %errorlevel% == 0 (
     echo Repository cloned successfully to user directory!
-    echo Creating desktop shortcut...
-    echo %userFolder%\Desktop\Aplikasi.bat
-    echo "!userFolder!\SIMRSKhanza\Aplikasi.bat"
     echo "Working Directory: !userFolder!\SIMRSKhanza"
     echo "Show in New Window"
     echo.
-    mklink /h "%userFolder%\Desktop\Aplikasi.bat" "!userFolder!\SIMRSKhanza\Aplikasi.bat"
-    if %errorlevel% == 0 (
-      echo Shortcut created successfully!
-      pause
-    ) else (
-      echo Shortcut creation failed! Error code: %errorlevel%
-      pause
-    )
-  ) else (
+    pause
+    start explorer !userFolder!\SIMRSKhanza\
+   ) else (
     echo Repository cloning failed! Error code: %errorlevel%
     pause
   )
